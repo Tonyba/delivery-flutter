00f6ea36fd9b44d2c969995a9a7a54d90647e969
Tony <37878200+Tonyba@users.noreply.github.com>
2024-10-09 20:06:18 -0400

    it will have to be modified according if in dev or prod mode

--- lib/global/enviroment.dart
+++ lib/global/enviroment.dart
@@ -1,12 +1,21 @@
-
 import 'dart:io';
 
-class Environment  {
-  static String apiUrl = Platform.isAndroid 
-  ? 'http://10.0.2.2:3000/api' 
-  : 'http://localhost:3000/api';
+// class Environment {
+//   static String apiUrl = Platform.isAndroid
+//       ? 'https://e55f-206-62-174-129.ngrok-free.app/api'
+//       : 'https://e55f-206-62-174-129.ngrok-free.app/api';
 
-  static String socketUrl = Platform.isAndroid 
-  ? 'http://10.0.2.2:3000' 
-  : 'http://localhost:3000';
+//   static String socketUrl = Platform.isAndroid
+//       ? 'https://e55f-206-62-174-129.ngrok-free.app'
+//       : 'https://e55f-206-62-174-129.ngrok-free.app';
+// }
+
+class Environment {
+  static String apiUrl = Platform.isAndroid
+      ? 'http://192.168.10.7:3000/api'
+      : 'http://192.168.10.7:3000/api';
+
+  static String socketUrl = Platform.isAndroid
+      ? 'http://192.168.10.7:3000'
+      : 'http://192.168.10.7:3000';
 }
